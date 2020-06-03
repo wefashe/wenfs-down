41c4772161464fe1caeccf7fd7a0889da5ff06d1
wefashe <wefashe@qq.com>
2020-06-03 09:17:54 +0800

    新建Bencode编码的测试类，添加测试用的种子文件

---
+++ src/test/java/org/github/bt/bencode/BencodeTest.java
@@ -0,0 +1,51 @@
+package org.github.bt.bencode;
+
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BencodeTest {
+
+    @Test
+    public void encode() throws IOException {
+
+        //字符串
+        String str = "i love coding!";
+
+        //数字
+        int i = 1234567890;
+
+        //列表
+        List<Object> list = new ArrayList<>();
+        list.add("列表1");
+        list.add("list 2");
+        list.add(str);
+        list.add(i);
+
+        //字典
+        Map<String, Object> map = new HashMap<>();
+        map.put("key1", "value2");
+        map.put("obj", list);
+        map.put("str", str);
+        map.put("i", i);
+
+        //编码
+        String encodeString = Bencode.encode(map);
+
+        System.out.println(encodeString);
+
+
+    }
+
+    @Test
+    public void decode() throws IOException {
+        String s = "d1:ii1234567890e4:key16:value23:objl7:列表16:list 214:i love coding!i1234567890ee3:str14:i love coding!e";
+        Map map = (Map) Bencode.decode(s);
+        System.out.println(map.toString());
+    }
+}
